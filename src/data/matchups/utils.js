554fd8c857e88cfe59d0204f2b799d68ebf0b4db
GuyHaviv37 <guyhavivcodes@gmail.com>
2021-09-15 12:17:07 +0300

    * Remove manifest.json link * Handle empty starter slots

--- src/data/matchups/utils.js
+++ src/data/matchups/utils.js
@@ -7,11 +7,19 @@ export const getLeaguesURLByUserId = (userId) => `https://api.sleeper.app/v1/use
 const decodeStarters = (starters) => {
     return starters.map((starter) => {
         const starterData = playersData[starter];
-        const starterName = starterData.full_name ?? starterData.team
-        return {
-            fullName: starterName,
-            position: starterData.position,
-            team: starterData.team
+        if (starterData) {
+            const starterName = starterData.full_name ?? starterData.team
+            return {
+                fullName: starterName,
+                position: starterData.position,
+                team: starterData.team
+            }
+        } else {
+            return {
+                fullName: 'Empty',
+                position: 'N/A',
+                team: 'N/A'
+            }
         }
     })
 }
@@ -31,8 +39,10 @@ export const addGameDataToStarters = (userStarters, teamData) => {
     for (const [leagueName, leagueObj] of Object.entries(userStarters)) {
         for (const starter of leagueObj.starters) {
             starter.leagueName = leagueName;
-            starter.gameDate = teamData[starter.team].gameDate;
-            starter.opponentString = teamData[starter.team].opponentString;
+            if (starter.team !== 'N/A') {
+                starter.gameDate = teamData[starter.team].gameDate;
+                starter.opponentString = teamData[starter.team].opponentString;
+            }
         }
     }
 }
